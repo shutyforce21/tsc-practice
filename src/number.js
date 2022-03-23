d026d83e52b8462271937c800488701e4e1fc1af
shuty <bbshuty.devlife@gmail.com>
2022-03-24 01:15:01 +0900

    to Void type section

---
+++ src/number.js
@@ -0,0 +1,8 @@
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10; // 2進数
+var octal = 484; // 8進数
+console.log(decimal);
+console.log(hex.toString(16));
+console.log(binary.toString(2));
+console.log(octal.toString(8));
---
+++ src/number.ts
@@ -0,0 +1,9 @@
+let decimal = 6;
+let hex = 0xf00d;
+let binary = 0b1010; // 2進数
+let octal = 0o744; // 8進数
+
+console.log(decimal);
+console.log(hex.toString(16));
+console.log(binary.toString(2));
+console.log(octal.toString(8));
